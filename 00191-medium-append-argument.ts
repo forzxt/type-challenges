d2d14023f71deb54f9b6c39e981e5c66c67ed22d
zhangxt@oiine.com <57976807+forzxt@users.noreply.github.com>
2023-04-14 01:57:52 +0800

    update

--- 00191-medium-append-argument.ts
+++ 00191-medium-append-argument.ts
@@ -1,19 +1,21 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
-type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
-type Result1 = (a: number, b: string, x: boolean) => number
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
 
-type Case2 = AppendArgument<() => void, undefined>
-type Result2 = (x: undefined) => void
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
 
 type cases = [
   Expect<Equal<Case1, Result1>>,
   Expect<Equal<Case2, Result2>>,
   // @ts-expect-error
   AppendArgument<unknown, undefined>,
-]
-
+];
 
 // ============= Your Code Here =============
-type AppendArgument<Fn extends (...args: any[]) => any, A> = 
+type AppendArgument<Fn extends (...args: any[]) => any, A> =
+  Fn extends (...args: infer T) => infer R
+  ? (...args: [...T, A]) => R
+  : (x: A) => void
--- 00459-medium-flatten.ts
+++ 00459-medium-flatten.ts
@@ -1,27 +1,21 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
 type cases = [
-  Expect<Equal<Flatten<[]>, []>>,
-  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
-  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-  Expect<
-    Equal<
-      Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>,
-      [{ foo: 'bar'; 2: 10 }, 'foobar']
-    >
-  >
-]
+    Expect<Equal<Flatten<[]>, []>>,
+    Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+    Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+    Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+    Expect<Equal<Flatten<[{ foo: "bar"; 2: 10 }, "foobar"]>, [{ foo: "bar"; 2: 10 }, "foobar"]>>,
+];
 
 // ============= Your Code Here =============
-type Flatten<T extends any[], Res extends any[] = []> = T extends [
-  infer F,
-  ...infer R
-]
-  ? R extends never
-    ? Res
-    : F extends any[]
-    ? Flatten<R, [...Res, ...F, R]>
-    : Flatten<R, [...Res, F, R]>
-  : T
+type Flatten<T extends any[], A extends any[] = []> = T extends [[[[infer P]]]]
+    ? [...A, P]
+    : T extends [[infer P]]
+    ? [...A, P]
+    : T extends [infer P]
+    ? [...A, P]
+    : T extends [infer F, ...infer R]
+    ? Flatten<R, [...A, F]>
+    : A;
